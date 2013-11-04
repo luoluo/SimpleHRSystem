365b66d190f8a8ece1004d4fde39590319b57f06
luoluo <luoluo1920@gmail.com>
2013-11-04 15:40:41 +0800

    student manage oop version done

--- HRSystem.cpp
+++ HRSystem.cpp
@@ -42,7 +42,7 @@ public:
 	int
 	writePeopleInfo(ofstream *peoout = NULL) {
 		if (peoout) {
-			cout << "write in people\n";
+//			cout << "write in people\n";
 			*peoout<< this -> name << "\t" << this -> index << "\t";
 		}
 		return 0;
@@ -89,134 +89,8 @@ Student ** students = new Student*[STUDENT];
 ofstream *Student::stuout = new ofstream("newstudentInfon.txt");
 int Student::total = 0;
 int totalstudent;
-int
-init() {
-	ifstream stuin;
-	stuin.open("studentInfo.txt"); 
-//		while (!stuin.eof()) {
-	if (stuin.is_open()) {
-		cout << "init start\n";
-		char name[100];
-		char index[100];
-		string classId;
-		while (!stuin.eof() &&
-			stuin >> name >> index >> classId) {
-			cout << "|" << name << "|" << index << "|" << classId<< endl;
-			students[totalstudent] = new Student(name, index, classId);
-			totalstudent++;
-		}
-		stuin.close();
-	}
-	cout << "init end\n";
-	return 0;
-}
-int
-display() {
-	int i;
-	for (i = 0; i < totalstudent; i++) {
-		students[i] -> showInfo();
-	}
-	return 0;
-}
-bool
-cmp(Student * s1, Student * s2) {
-//	return strcmp(s1 -> classId, s2 -> classId ) > 0;
-	return s1->classId.compare(s2->classId) > 0;
-}
-int
-stusort() {
-	sort(students, students+totalstudent, cmp);
-	return 0;
-}
 
-int searchByClass(string classIdx) {
-	int i;
-	for (i = 0; i < totalstudent; i++) {
-		if (classIdx.compare(students[i]->classId) == 0) {
-			return i;
-		}
-	}
-	return -1;
-}
-int
-search() {
-	string classIdx;
-	cin >> classIdx;
-	int i = searchByClass(classIdx);
-	if (i == -1) {
-		cout << "not here\n";
-	} else {
-		cout << i << endl;
-	}
-	return 0;
-}
-int
-edit() {
-	string classIdx;
-	cin >> classIdx;
-	int i = searchByClass(classIdx);
-	if (i == -1) {
-		cout << "not here\n";
-	} else {
-		string newclassId;
-		cin >> newclassId;
-		students[i] -> classId = newclassId;
-		cout << i << endl;
-	}
-	return 0;	
-}
-int
-add() {
-	char name[100];
-	char index[100];
-	string classId;
-	cin >> name >> index >> classId;
-	students[totalstudent] = new Student(name, index, classId);
-	totalstudent++;
-	return 0;
-}
-int
-remove() {
-	string classIdx;
-	cin >> classIdx;
-	int i = searchByClass(classIdx);
-	if (i == -1) {
-		cout << "not here\n";
-	} else {
-		int j;
-		for (j = i+1; j < totalstudent; j++) {
-			students[j-1] = students[j];
-		}
-		totalstudent--;
-	}
-	return 0;
-}
-int
-goback() {
-	printf("returning...\n");
-	return 0;
-}
 
-int
-studentManage() {
-	while (1) {
-		int x;
-		printf("1->display\t2->sort\t3->search\t4.edit\t5.add\t6.remove\t0->back\n");
-		scanf("%d", &x);
-		switch(x) {
-//			case 1: Student::sdisplay(); break;
-			case 1: display(); break;
-			case 2: stusort(); break;
-			case 3: search(); break;
-			case 4: edit(); break;
-			case 5: add(); break;
-			case 6: remove(); break;
-			case 0: goback(); return 0;
-			default: ;
-		}
-	}
-	return 0;
-}
 int
 teacherManage() {
 	return 0;
@@ -229,48 +103,219 @@ int
 teachAssistManage() {
 	return 0;
 }
-int
-logout() {
-	//write back
-	int i;
-	for (i = 0; i < totalstudent; i++) {
-		students[i] -> writeStudentInfo();
+
+class StudentManager {
+private:
+	static StudentManager *_instance;
+	static Student **students;
+	static int totalstudent;
+	StudentManager() {
 	}
-	Student::stuout -> close();
-	rename("newstudentInfon.txt", "studentInfo.txt");
-	printf("lt\n");
-	return 0;
-}
-int
-operation() {
-	while (1) {
-		int x;
-		printf("1->student\t2->teacher\t3->graduate\t4.TA\t0->lt\n");
-		scanf("%d", &x);
-		switch(x) {
-			case 1: studentManage(); break;
-			case 2: teacherManage(); break;
-			case 3: graduateManage(); break;
-			case 4: teachAssistManage(); break;
-			case 0: logout(); return 0;
-			default: ;
+public:
+	static StudentManager *
+	getManager() {
+		if (_instance == NULL) {
+			_instance = new StudentManager();	
+			students = new Student*[STUDENT];
+			init();
+		}
+		return _instance;
+	}
+	static int
+	init() {
+		ifstream stuin;
+		stuin.open("studentInfo.txt"); 
+	//		while (!stuin.eof()) {
+		if (stuin.is_open()) {
+			cout << "init start\n";
+			char name[100];
+			char index[100];
+			string classId;
+			while (!stuin.eof() &&
+				stuin >> name >> index >> classId) {
+				cout << "|" << name << "|" << index << "|" << classId<< endl;
+				students[totalstudent] = new Student(name, index, classId);
+				totalstudent++;
+			}
+			stuin.close();
+		}
+		cout << "init end\n";
+		return 0;
+	}
+	static bool
+	cmp(Student * s1, Student * s2) {
+	//	return strcmp(s1 -> classId, s2 -> classId ) > 0;
+		return s1->classId.compare(s2->classId) > 0;
+	}
+	static int
+	stusort() {
+		sort(students, students+totalstudent, cmp);
+		return 0;
+	}
+	
+	static int searchByClass(string classIdx) {
+		int i;
+		for (i = 0; i < totalstudent; i++) {
+			if (classIdx.compare(students[i]->classId) == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	static int
+	search() {
+		string classIdx;
+		cin >> classIdx;
+		int i = searchByClass(classIdx);
+		if (i == -1) {
+			cout << "not here\n";
+		} else {
+			cout << i << endl;
+		}
+		return 0;
+	}
+	static int
+	edit() {
+		string classIdx;
+		cin >> classIdx;
+		int i = searchByClass(classIdx);
+		if (i == -1) {
+			cout << "not here\n";
+		} else {
+			string newclassId;
+			cin >> newclassId;
+			students[i] -> classId = newclassId;
+			cout << i << endl;
+		}
+		return 0;	
+	}
+	static int
+	add() {
+		char name[100];
+		char index[100];
+		string classId;
+		cin >> name >> index >> classId;
+		students[totalstudent] = new Student(name, index, classId);
+		totalstudent++;
+		return 0;
+	}
+	static int
+	remove() {
+		string classIdx;
+		cin >> classIdx;
+		int i = searchByClass(classIdx);
+		if (i == -1) {
+			cout << "not here\n";
+		} else {
+			int j;
+			for (j = i+1; j < totalstudent; j++) {
+				students[j-1] = students[j];
+			}
+			totalstudent--;
+		}
+		return 0;
+	}
+	static int
+	goback() {
+		printf("returning...\n");
+		return 0;
+	}
+	static int
+	display() {
+		int i;
+		for (i = 0; i < totalstudent; i++) {
+			students[i] -> showInfo();
+		}
+		return 0;
+	}
+	static int
+	manage() {
+		while (1) {
+			int x;
+			printf("1->display\t2->sort\t3->search\t4.edit\t5.add\t6.remove\t0->back\n");
+			scanf("%d", &x);
+			switch(x) {
+				case 1: display(); break;
+				case 2: stusort(); break;
+				case 3: search(); break;
+				case 4: edit(); break;
+				case 5: add(); break;
+				case 6: remove(); break;
+				case 0: goback(); return 0;
+				default: ;
+			}
+		}
+		return 0;
+	}
+	static int 
+	writeback() {
+		int i;
+		for (i = 0; i < totalstudent; i++) {
+			students[i] -> writeStudentInfo();
+		}
+		Student::stuout -> close();
+		rename("newstudentInfon.txt", "studentInfo.txt");
+	}
+};
+StudentManager * StudentManager::_instance = NULL;
+Student ** StudentManager::students = NULL;
+int StudentManager::totalstudent = 0;
+
+class Manager {
+private:
+	static Manager *_instance;
+	static StudentManager * studentManager;
+	Manager() {
+	}
+public:
+	static Manager *
+	getManager() {
+		if (_instance == NULL) {
+			_instance = new Manager();	
+			studentManager = StudentManager::getManager();
+		}
+		return _instance;
+	}
+	static int
+	operation() {
+		while (1) {
+			int x;
+			printf("1->student\t2->teacher\t3->graduate\t4.TA\t0->lt\n");
+			scanf("%d", &x);
+			switch(x) {
+				case 1:	studentManager->manage(); break;
+		//		case 2: teacherManage(); break;
+		//		case 3: graduateManage(); break;
+		//		case 4: teachAssistManage(); break;
+				case 0: logout(); return 0;
+				default: ;
+			}
 		}
 	}
-
-}
-
+	static int
+	logout() {
+		//write back
+		studentManager->writeback();
+		printf("lt\n");
+		return 0;
+	}
+};
+Manager * Manager::_instance = NULL;
+StudentManager * Manager::studentManager = NULL;
 int
 test() {
 //	People* p = new People("lili", "000", "M", "12345678", 2008, 10, 12);
 //	p -> showInfo();
 //	Student *s = new Student("lili", "000", "M", "12345678", 2008, 10, 12, "2013");
 //	s -> showInfo();
-	cout << Student::total << endl;
+//	cout << Student::total << endl;
+	Manager *manager = Manager::getManager();
+	manager -> operation();
 }
 int
 main () {
 	test();
-	init();
-	operation();
+//	init();
+//	operation();
 	return 0;
 }
